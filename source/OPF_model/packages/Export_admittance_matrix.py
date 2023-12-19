db16c874a984237b74c1dd035d79b92d3b8d589e
Abel Johnson <abeljohnson207@gmail.com>
2023-12-20 00:19:14 +0530

    Delete source directory

--- source/OPF_model/packages/Export_admittance_matrix.py
+++
@@ -1,7 +0,0 @@
-def GetAdmittanceMatrix(dss, OPF_model_path, os):
-    dss.text("set datapath=" + OPF_model_path + '/AuxData')
-
-    file1 = dss.text('Export YPrims')
-    file2 = dss.text('Export Y')
-    os.replace(file1, "LV_Network_EXP_YPRIM.CSV")
-    os.replace(file2, "LV_Network_EXP_Y.CSV")
